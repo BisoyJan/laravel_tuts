54c227abf28f9cb1b49353b5c06d8a4dfbe5de20
Jan Ramil Intong <bisoyjan@gmail.com>
2024-01-08 21:12:47 +0800

    updates

---
+++ l10-task-list/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<h1>
+    The list of tasks
+</h1>
+
+
+<div>
+    <!-- @if (count($tasks)) -->
+    @forelse ($tasks as $task)
+    <div>{{$task->title}}</div>
+    @empty
+    <div>There are no tasks!</div>
+    @endforelse
+    <!-- @endif -->
+</div>
--- l10-task-list/routes/web.php
+++ l10-task-list/routes/web.php
@@ -13,22 +13,83 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Main Page';
+
+class Task
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $description,
+        public ?string $long_description,
+        public bool $completed,
+        public string $created_at,
+        public string $updated_at
+    ) {
+    }
+}
+
+$tasks = [
+    new Task(
+        1,
+        'Buy groceries',
+        'Task 1 description',
+        'Task 1 long description',
+        false,
+        '2023-03-01 12:00:00',
+        '2023-03-01 12:00:00'
+    ),
+    new Task(
+        2,
+        'Sell old stuff',
+        'Task 2 description',
+        null,
+        false,
+        '2023-03-02 12:00:00',
+        '2023-03-02 12:00:00'
+    ),
+    new Task(
+        3,
+        'Learn programming',
+        'Task 3 description',
+        'Task 3 long description',
+        true,
+        '2023-03-03 12:00:00',
+        '2023-03-03 12:00:00'
+    ),
+    new Task(
+        4,
+        'Take dogs for a walk',
+        'Task 4 description',
+        null,
+        false,
+        '2023-03-04 12:00:00',
+        '2023-03-04 12:00:00'
+    ),
+];
+
+
+Route::get('/', function () use ($tasks) {
+    return view('index', [
+        'tasks' => $tasks
+    ]);
 });
 
-Route::get('/hello', function () {
+Route::get('/xxx', function () {
     return 'Hello';
 })->name('hello');
 
 Route::get('/hallo', function () {
-    return redirect('/hello');
+    return redirect()->route('hello');
 });
 
 Route::get('/greet/{name}', function ($name) {
     return 'Hello ' . $name . '!';
 });
 
+//Fallback route when the link is not find in the above routes this method will be executed
+Route::fallback(function () {
+    return 'Not Found';
+});
 // GET
 // POST
 // PUT
