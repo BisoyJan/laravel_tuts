512cbb17738a4734bf59abbba4544bbcaf7129a8
Jan Ramil Intong <bisoyjan@gmail.com>
2024-01-14 21:43:13 +0800

    Layouts Using Template Inheritance

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -1,16 +1,15 @@
-<h1>
-    The list of tasks
-</h1>
+@extends('layouts.app')
 
+@section('title', 'The list of tasks')
 
-<div>
+@section('content')
     <!-- @if (count($tasks)) -->
     @forelse ($tasks as $task)
-    <div>
-        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
-    </div>
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
     @empty
-    <div>There are no tasks!</div>
+        <div>There are no tasks!</div>
     @endforelse
     <!-- @endif -->
-</div>
+@endsection
