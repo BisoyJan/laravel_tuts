c506f601df20201a84c4920b76ffe56c7daf59ee
BisoyJan <bisoyjan@gmail.com>
2024-02-28 21:34:20 +0800

    Models and Migrations

--- book-review/database/migrations/2024_02_23_084656_create_books_table.php
+++ book-review/database/migrations/2024_02_23_084656_create_books_table.php
@@ -12,8 +12,10 @@ return new class extends Migration {
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+
             $table->string('title');
             $table->string('author');
+
             $table->timestamps();
         });
     }
