5d2f397f2880dad1eb2312682edaaf8e294d05f3
Jan Ramil Intong <bisoyjan@gmail.com>
2024-07-25 16:05:49 +0800

    Added data valdiation

--- livewire-poll/app/Livewire/CreatePoll.php
+++ livewire-poll/app/Livewire/CreatePoll.php
@@ -2,12 +2,23 @@
 
 namespace App\Livewire;
 
+use App\Models\Poll;
 use Livewire\Component;
 
 class CreatePoll extends Component
 {
     public $title;
-    public $options = [''];
+    public $options = ['First'];
+
+    protected $rules = [
+        'title' => 'required|min:3|max:255',
+        'options' => 'required|array|min:1|max:10',
+        'options.*' => 'required|min:1|max:255'
+    ];
+
+    protected $messages = [
+        'options.*' => 'The Option must be between 1 and 255 characters',
+    ];
     
     public function render()
     {
@@ -18,4 +29,37 @@ class CreatePoll extends Component
     {
         $this->options[] = '';
     }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        $this->options = array_values($this->options);
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function createPoll()
+    {
+
+        $this->validate();
+
+        #Refactored Code
+        $poll = Poll::create([
+            'title' => $this->title
+        ])->options()->createMany(
+            collect($this->options)
+                ->map(fn($options) => ['name' => $options])
+                ->all()
+            );
+
+        // foreach ($this->options as $optionsName){
+        //     $poll->options()->create(['name' => $optionsName]);
+        // }
+
+        $this->reset(['title', 'options']);
+    }
+    
 }
