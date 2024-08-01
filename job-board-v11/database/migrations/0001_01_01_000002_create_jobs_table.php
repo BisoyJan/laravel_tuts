dff4a8b5e84c45a58230aa1698c6b3a78d5a0e72
Jan Ramil Intong <bisoyjan@gmail.com>
2024-08-01 20:36:27 +0800

    seeders

--- job-board-v11/database/migrations/0001_01_01_000002_create_jobs_table.php
+++ job-board-v11/database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        Schema::create('queue_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('queue')->index();
             $table->longText('payload');
@@ -50,7 +49,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('queue_jobs');
         Schema::dropIfExists('job_batches');
         Schema::dropIfExists('failed_jobs');
     }
