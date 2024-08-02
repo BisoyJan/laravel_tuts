ec8d8c2372a139ce827f2e4c3f186831145991c8
Jan Ramil Intong <bisoyjan@gmail.com>
2024-08-02 23:09:06 +0800

    added search forms

--- job-board-v11/resources/views/job/index.blade.php
+++ job-board-v11/resources/views/job/index.blade.php
@@ -3,8 +3,18 @@
 
     <x-card class="mb-4 text-sm">
         <div class="mb-4 grid grid-cols-2 gap-4">
-            <div>1</div>
-            <div>2</div>
+            <div>
+                <div class="mb-1 font-semibold">Search</div>
+                <x-text-input name="search" value="" placeholder="Search anything" />
+            </div>
+            <div>
+                <div class="mb-1 font-semibold">Salary</div>
+
+                <div class="flex space-x-2">
+                    <x-text-input name="min_salary" value="" placeholder="From" />
+                    <x-text-input name="max_salary" value="" placeholder="To" />
+                </div>
+            </div>
             <div>3</div>
             <div>4</div>
         </div>
