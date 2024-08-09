644924a8f414e19662ead6c82282246c27e0f8b4
BisoyJan <bisoyjan@gmail.com>
2024-08-09 10:07:14 +0800

    Added Delete function

--- job-board-v11/app/Http/Controllers/MyJobApplicationController.php
+++ job-board-v11/app/Http/Controllers/MyJobApplicationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JobApplication;
 use Illuminate\Http\Request;
 
 class MyJobApplicationController extends Controller
@@ -68,8 +69,15 @@ class MyJobApplicationController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(JobApplication $myJobApplication)
     {
-        //
+
+        //dd($myJobApplication);// this is to troubleshooting
+        $myJobApplication->delete();
+
+        return redirect()->back()->with(
+            'success',
+            'Job application removed'
+        );
     }
 }
